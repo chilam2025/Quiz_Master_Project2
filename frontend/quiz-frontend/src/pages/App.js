35bf6abd36fd911903112a216832c130f5e534f2
miniiminz <zda23b007@iitmz.ac.in>
2025-11-27 02:29:22 +0300

    Added app file

---
+++ frontend/quiz-frontend/src/pages/App.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import AuthPage from "./AuthPage";
+import QuizList from "./QuizList";
+import QuizPage from "./QuizPage";
+import ResultsPage from "./ResultsPage";
+
+// ProtectedRoute ensures only logged-in users can access certain pages
+function ProtectedRoute({ children }) {
+  const user = JSON.parse(localStorage.getItem("user"));
+  if (!user || !user.token) {
+    return <Navigate to="/" replace />;
+  }
+  return children;
+}
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* Auth Page */}
+        <Route path="/" element={<AuthPage />} />
+
+        {/* Quizzes List (Protected) */}
+        <Route
+          path="/quizzes"
+          element={
+            <ProtectedRoute>
+              <QuizList />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Single Quiz Page (Protected) */}
+        <Route
+          path="/quiz/:id"
+          element={
+            <ProtectedRoute>
+              <QuizPage />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Results Page (Protected) */}
+        <Route
+          path="/results/:user_id/:quiz_id"
+          element={
+            <ProtectedRoute>
+              <ResultsPage />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Redirect unknown routes */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
