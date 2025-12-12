fd2ba221e529c5e2f2fc968a67d8d14b35f84092
Ahlam <zda23b026@iitmz.ac.in>
2025-12-12 07:00:38 +0300

    added a render backend link to Prediction.js for connection

--- frontend/quiz-frontend/src/pages/Prediction.js
+++ frontend/quiz-frontend/src/pages/Prediction.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from "recharts";
 
-const API_URL = "http://127.0.0.1:5000";
+const API_URL = "https://quiz-master-project2-backend.onrender.com";
 
 export default function Prediction() {
   const navigate = useNavigate();
